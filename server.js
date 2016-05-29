b86e2340ba266782e8659123584afeb0984aa1ce
Shruti Sinha <shruti2809@gmail.com>
2016-05-29 15:39:28 +0530

    display quotes in unlisted form

--- server.js
+++ server.js
@@ -13,11 +13,17 @@ MongoClient.connect('mongodb://star-wars-quotes:starwars@ds019143.mlab.com:19143
     console.log('listening on 3000')
   })
 })
+
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 // All your handlers here...
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.render('index.ejs', {quotes: result})
+  })
 })
 // Note: __dirname is the path to your current working directory. Try logging it and see what you get!
 app.post('/quotes', (req, res) => {
