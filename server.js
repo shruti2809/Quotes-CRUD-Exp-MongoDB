c4290bb11dacb6f2d637b12aa6e32fa8e3381105
Shruti Sinha <shruti2809@gmail.com>
2016-05-29 12:56:22 +0530

    Handle GET requests

--- server.js
+++ server.js
@@ -3,4 +3,7 @@ const app = express();
 app.listen(3000, function() {
   console.log('listening on 3000')
 })
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 console.log('May Node be with you! :)')
